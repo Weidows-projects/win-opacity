18ff04466e965e83426665a3d62f0507c6893f7b
Mike Cluck <mcluck90@gmail.com>
2018-05-10 14:08:57 -0600

    Added JSDoc and comments

--- lib.js
+++ lib.js
@@ -9,15 +9,34 @@ const LWA_ALPHA = 0x2;
 const WS_EX_LAYERED = 0x80000;
 const WS_EX_TOOLWINDOW = 0x00000080;
 
+/**
+ * A window handle. Essentially, a pointer
+ * @typedef {number} WindowHandle
+ */
+
+/**
+ * Contains information about a window
+ * @typedef {Object} NativeWindow
+ * @property {string} title - Title on the window
+ * @property {WindowHandle} handle - The actual handle pointing to the window
+ */
+
+/**
+ * Can a window be accessed via Alt+Tab?
+ * @param {WindowHandle|NativeWindow} handle The window object or handle
+ */
 const isAltTabWindow = (handle) => {
+  // If it's a window object, switch it to just the handle
   if (handle.handle) {
     handle = handle.handle;
   }
 
+  // First see if the window is considered visible
   if (!user32.IsWindowVisible(handle)) {
     return false;
   }
 
+  // Climb up through the layout tree to determine if it's root is visible
   let handleWalk = null;
   let attemptedHandle = user32.GetAncestor(handle, GA_ROOTOWNER);
   while (attemptedHandle !== handleWalk) {
@@ -27,6 +46,7 @@ const isAltTabWindow = (handle) => {
       break;
     }
   }
+
   if (handleWalk !== handle) {
     return false;
   }
@@ -39,50 +59,75 @@ const isAltTabWindow = (handle) => {
   return true;
 };
 
+// Stores all of the windows found in the getWindows function
 let _windows = [];
-const getWindows = ffi.Callback('bool', ['long', 'int32'], function(hwnd) {
-  if (!isAltTabWindow(hwnd)) {
+
+// Called during EnumWindows
+const getWindows = ffi.Callback('bool', ['long', 'int32'], 
+  /**
+   * @param {WindowHandle} hwnd
+   */
+  function(hwnd) {
+    if (!isAltTabWindow(hwnd)) {
+      return true;
+    }
+
+    // Load window title
+    const buffer = new Buffer(255);
+    user32.GetWindowTextW(hwnd, buffer, 255);
+    const windowName = buffer.toString('utf16le').replace(/\u0000/g, '').trim();
+
+    if (!windowName) {
+      return true;
+    }
+
+    // Provide the windows title and original handle
+    _windows.push({
+      title: windowName,
+      handle: hwnd
+    });
     return true;
-  }
-
-  // Load window title
-  const buffer = new Buffer(255);
-  user32.GetWindowTextW(hwnd, buffer, 255);
-  const windowName = buffer.toString('utf16le').replace(/\u0000/g, '').trim();
-
-  if (!windowName) {
-    return true;
-  }
-
-  _windows.push({
-    title: windowName,
-    handle: hwnd
   });
-  return true;
-});
 
+/**
+ * Returns all visible windows
+ * @returns {NativeWindow[]}
+ */
 exports.getWindows = function() {
   _windows = [];
   user32.EnumWindows(getWindows, 0);
   return _windows;
 };
 
+/**
+ * Sets the opacity of a given window
+ * @param {NativeWindow|WindowHandle} handle 
+ * @param {number} opacity Must be [0-255]
+ */
 exports.setOpacity = function(handle, opacity) {
   if (handle.title && handle.handle) {
     handle = handle.handle;
   }
 
+  // Make sure the opacity is a valid value
   if (typeof opacity !== 'number' || opacity < 0 || opacity > 255) {
     throw new Error('Must provide an opacity value between 0 and 255');
   }
 
+  // Take the floor of the number to remove any floating point bits
   opacity = opacity | 0;
 
+  // Set the opacity
   const windowLong = user32.GetWindowLongA(handle, GWL_EXSTYLE);
   user32.SetWindowLongA(handle, GWL_EXSTYLE, windowLong | WS_EX_LAYERED);
   return user32.SetLayeredWindowAttributes(handle, 0, opacity, LWA_ALPHA);
 };
 
+/**
+ * Returns the opacity of a window
+ * @param {NativeWindow|WindowHandle} handle 
+ * @returns {number} Number is [0-255]
+ */
 exports.getOpacity = function(handle) {
   if (handle.title && handle.handle) {
     handle = handle.handle;
